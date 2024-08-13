893e91450717d46c52b9e7d7da38dbfd3b708c18
vinaneosoft <vinaneosoft@gmail.com>
2024-08-13 18:20:48 +0530

    commit

---
+++ src/app/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './App.css';
+import { Header } from '../header/Header';
+
+function App() { // capital case
+// java logic
+
+
+  return (
+    <React.Fragment> 
+      <h1 className="mainHeading">EMPLOYEE MANAGEMENT</h1>
+      <Header></Header>  
+      <div>
+
+
+      </div>
+     {/*  footer component*/}
+    </React.Fragment>
+  );
+}
+
+export default App;
+
+
+function add(){ // camel case
+
+}
---
+++ src/header/Header.js
@@ -0,0 +1,10 @@
+import "./Header.css";
+
+export function Header(){
+
+    return(
+        <header className="myHeader">
+            <h2>EMPLOYEE CRUD PROJECT</h2>
+        </header>
+    );
+}
